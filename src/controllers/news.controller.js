a6d69d728cf0e7e885593fe9cd3429509601d8eb
Cleiss <cleitonsns@gmail.com>
2023-10-18 18:22:18 -0300

    feat: paginação

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -3,7 +3,7 @@ import newsService from '../services/news.service.js'
 const create = async (req, res) => {
 
     try {
-        const {title, text, banner} = req.body
+        const { title, text, banner } = req.body
 
         if (!title || !text || !banner) {
             res.status(400).send({
@@ -11,7 +11,7 @@ const create = async (req, res) => {
             })
         }
 
-        await newsService.createService ({
+        await newsService.createService({
             title,
             text,
             banner,
@@ -21,22 +21,66 @@ const create = async (req, res) => {
         res.sendStatus(201)
 
     } catch (erro) {
-        res.status(500).send({message: erro.message})
+        res.status(500).send({ message: erro.message })
     }
 
 }
 
 const findAll = async (req, res) => {
-    const news = await newsService.findAllService()
+
+    let { limit, offset } = req.query
+
+    limit = Number(limit)
+    offset = Number(offset)
+
+    if (!limit) {
+        limit = 5
+    }
+
+    if (!offset) {
+        offset = 0
+    }
+
+    const news = await newsService.findAllService(offset, limit)
+    const total = await newsService.countNews()
+    const currentUrl = req.baseUrl
+
+    const next = offset + limit
+    /* next é basicamente a ''próxima URL'' da paginação */
+
+    const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${offset}` : null
+    /* currentUrl neste caso é '/news' */
+
+    const previous = offset - limit < 0 ? null : offset - limit
+
+    const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null
 
     if (news.length === 0) {
         return res.status(400).send({ message: 'não há notícias cadastradas' })
     }
-    
-    res.send(news)
+
+    res.send({
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total,
+        results: news.map((item) => ({
+            id: item._id,
+            title: item.title,
+            text: item.text,
+            banner: item.banner,
+            likes: item.likes,
+            comments: item.comments,
+            name: item.user.name,
+            userName: item.user.username,
+            userFoto: item.user.foto
+
+        }))
+    })
 }
 
-export default {create, findAll}
+export default { create, findAll }
 
 /* 
     newsService é o nome do 'pacote' que contém as variáveis exportadas.
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -2,6 +2,14 @@ import News from '../models/News.js'
 
 const createService = (body) => News.create(body)
 
-const findAllService = () => News.find()
+const findAllService = (offset, limit) => News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate('user')
+/*
+ _id = -1: ordena desde o último id criado até o primeiro da lista; inverte a ordem.
+skip = de quanto em quanto será o intervalo. começa do offset e vai somando a partir do limit.
+populate = traz informações do argumento que for utilizado. neste caso é 'user'.
+*/
 
-export default { createService, findAllService }
+const countNews = () => News.countDocuments()
+/* conta quantos documentos há na collection no BD */
+
+export default { createService, findAllService, countNews }
