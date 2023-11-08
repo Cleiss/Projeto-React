68a2dffdf3638e907fb4e2e1ce770e0a1d8d7e2a
cleiss <cleitonsns@gmail.com>
2023-11-07 21:32:07 -0300

    dir changed

---
+++ front/spa-blog-react/src/Datas.js
@@ -0,0 +1,32 @@
+const news = [
+    {
+        title: "noticia 1",
+        text: "texto texto texto texto texto texto texto texto texto texto",
+        image: "https://www.webdevdrops.com/geradores-de-textos-e-imagens-lorem-ipsum-2f39503f63c1/images/lorem-4.jpg",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "noticia 2",
+        text: "texto texto texto texto texto texto texto texto texto texto",
+        image: "https://www.webdevdrops.com/geradores-de-textos-e-imagens-lorem-ipsum-2f39503f63c1/images/lorem-4.jpg",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "noticia 3",
+        text: "texto texto texto texto texto texto texto texto texto texto",
+        image: "https://www.webdevdrops.com/geradores-de-textos-e-imagens-lorem-ipsum-2f39503f63c1/images/lorem-4.jpg",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "noticia 4",
+        text: "texto texto texto texto texto texto texto texto texto texto",
+        image: "https://www.webdevdrops.com/geradores-de-textos-e-imagens-lorem-ipsum-2f39503f63c1/images/lorem-4.jpg",
+        likes: 2,
+        comments: 4
+    }
+]
+
+export default news
