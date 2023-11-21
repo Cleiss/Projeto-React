b4627085bd918def5f2165f69f693957156d02f6
cleiss <cleitonsns@gmail.com>
2023-11-21 18:46:33 -0300

    feat: globalstyled

--- front/spa-blog-react/src/App.jsx
+++ front/spa-blog-react/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import {Home} from "./Pages/Home/Home"
+import { GlobalStyled } from './GlobalStyled.jsx'
 
 /* essa função é um Component. Components são funções que retornam HTML */
 export function App() {
 
 
   return (
+    <>
+    <GlobalStyled />
     <Home />
+    </>
   )
 }
 
