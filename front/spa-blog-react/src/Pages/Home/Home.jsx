fb12aa368edcc7551f6f0552bdbbbc28231f0c3e
cleiss <cleitonsns@gmail.com>
2023-11-22 22:17:21 -0300

    feat: AXIOS

--- front/spa-blog-react/src/Pages/Home/Home.jsx
+++ front/spa-blog-react/src/Pages/Home/Home.jsx
@@ -2,12 +2,18 @@ import { Navbar } from "../../Components/Navbar/Navbar.jsx"
 import { Card } from "../../Components/Card/Card.jsx"
 import { news } from "../../Datas.js"
 import { HomeBody } from "./HomeStyled.jsx"
+import { getAllPosts } from "../../Services/postsServices.js"
 
 export function Home() {
-    /*
-    return apenas retorna uma única tag ou fragment, então todas as outras precisam estar envolvidas/aninhadas numa só.
-    fragment é uma tag vazia. não necessariamente precisa ser uma section, por exemplo, ou algo do tipo.
-    */
+
+    async function findAllPosts() {
+        const response = await getAllPosts()
+
+        console.log(response.data.results)
+    }
+
+    findAllPosts()
+
     return (
 
         <section>
@@ -24,4 +30,10 @@ export function Home() {
     )
 }
 
-/* .map é um HOF (high order function). pesquisar sobre. */
+/* 
+    .map é um HOF (high order function). pesquisar sobre.
+
+    return apenas retorna uma única tag ou fragment, então todas as outras precisam estar envolvidas/aninhadas numa só.
+    fragment é uma tag vazia. não necessariamente precisa ser uma section, por exemplo, ou algo do tipo.
+    
+*/
