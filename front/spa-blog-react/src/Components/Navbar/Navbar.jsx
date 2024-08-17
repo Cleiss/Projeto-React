3ff589a161366f60ba39c817d35caacca1a89fd6
cleiss <cleitonsns@gmail.com>
2024-08-17 16:36:27 -0300

    Validação de input com React Hook Form e Zod

--- front/spa-blog-react/src/Components/Navbar/Navbar.jsx
+++ front/spa-blog-react/src/Components/Navbar/Navbar.jsx
@@ -2,18 +2,38 @@ import { Link, Outlet, useNavigate } from "react-router-dom"
 import logo from "../../images/LogoBN.png"
 import {Button, Nav, ImgLogo, InputSearch} from "../Navbar/NavbarStyled.jsx"
 import {useForm} from "react-hook-form"
+import {z} from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
+
+const searchSchema = z.object({
+    title: z.string().nonempty({message:"Pesquise por um título"}).refine(value => !/^\s*$/.test(value), {message:"Pesquise por um título"})
+})
+
+/* refine(value => !/^\s*$/.test(value)) testa se há apenas espaço na espaço e
+     está negando para que não deixe ter apenas espaço */
 
 export function Navbar() {
 
-    const {register, handleSubmit, reset} = useForm()
+    /*constante descontruída com origem no hook form*/
+    /*register: registra cada input; handleSubmit: método para o 'submit'*/
+    const {register, 
+        handleSubmit, 
+        reset,
+        formState: {errors}
+        } = useForm({
+        resolver: zodResolver(searchSchema)
+    }) 
+
     const navigate = useNavigate()
 
     function onSearch (data) {
         const {title} = data
         navigate(`/search/${title}`)
-        reset()
+        reset() /* limpa automaticamente o campo de pesquisa após pesquisar/navegar */
     }
 
+    /* input precisa estar dentro de um form */
+    /* '...' = spread operator (pega vários dados): '...register' */
     return (
         <>
             <Nav>
@@ -36,6 +56,7 @@ export function Navbar() {
 
                 <Button>Entrar</Button>
             </Nav>
+            {errors.title && <span>{errors.title.message}</span>}
             <Outlet/>
         </>
     )
