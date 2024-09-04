22f3a81132bce674a4e91a7f8bdb68492576d0a0
Cleiss <cleitonsns@gmail.com>
2024-09-03 23:19:50 -0300

    adição de busca por username

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,9 +6,11 @@ const findAllUsersService = () => User.find().sort({ _id: -1 })
 
 const findUserIdService = (id) => User.findById(id)
 
+const findUsernameService = (username) => User.find(username)
+
 const findUserEmailService = (email) => User.findOne(email)
 
 const updtUserService = (id, nome, username, email, senha, foto, background) => User.findOneAndUpdate({ _id: id }/*encontra pelo id*/, { nome, username, email, senha, foto, background } /*parâmetros que se quer atualizar*/)
 
 
-export default { createUserService, findAllUsersService, findUserIdService, findUserEmailService, updtUserService }
+export default { createUserService, findAllUsersService, findUserIdService, findUsernameService, findUserEmailService, updtUserService }
